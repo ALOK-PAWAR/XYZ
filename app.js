b38655eb0c4f0a2f5c6c8794f1b55ffc2dbe633f
ALOK-PAWAR <168816493+ALOK-PAWAR@users.noreply.github.com>
2024-09-26 22:29:25 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+function showPractical(practicalNumber) {
+    alert("You clicked on Practical " + practicalNumber);
+    // Here you can add logic to handle what happens when a practical button is clicked
+    // For example, you could load specific sections of the PDF or perform other actions
+}
